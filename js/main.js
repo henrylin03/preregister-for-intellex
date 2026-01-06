199283901e180cc20b6cc021dacd1abb8cd8e367
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-07 08:43:26 +1100

    feat: remove language switch
    cannot determine the reason why the language switch (and its ability to toggle between English and Simplified Chinese using localStorage) works in development server but not in production.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-import {
-  enableLanguageSwitch,
-  renderPageInPreferredLanguage,
-} from "./languageManager.mjs";
 import enableCTAs from "./enableCTAs.mjs";
 
 const isHomepage = () => window.location.pathname === "/";
 
 if (isHomepage()) enableCTAs();
-enableLanguageSwitch();
-renderPageInPreferredLanguage();
